1ebe96d2dcdb1e4e4774858f1c6273f80b40f6a3
Francesco <francosco.galleano@osteocom.me>
2019-02-17 22:03:53 +0100

    update 0.1.3

--- fields/_core.ts
+++ fields/_core.ts
@@ -11,48 +11,55 @@ import * as models from "../models/_lib"
 
 import * as validator from "./validator/_lib"
 
-export type InputType = "string" | "password" | "mail" | "boolean" | "radio" | "upload" | "uploadImage" | "uploadPdf" | "number" | "combo"
+export type InputType = "string" | "password" | "mail" | "boolean" | "radio" | "upload" | "uploadImage" | "uploadPdf" | "number" | "combo" | "date"
 
 
-export interface InputArgs<T> {
-    type: InputType
+export interface InputArgs {
     title: string
     description?: string
     nullable?: boolean
 }
 
 
-export interface StringInput extends InputArgs<string> {
+export interface StringInput extends InputArgs {
+    type: "string" | "password" | "mail"
     multiline?: number
     password?: boolean
     minLength?: number
     maxLength?: number
 }
 
-export interface NumberInput extends InputArgs<string> {
+export interface NumberInput extends InputArgs {
+    type: "number"
     max?: number
     min?: number
 }
 
 
-export interface DateInput extends InputArgs<string> {
+export interface DateInput extends InputArgs {
+    type: "date"
     max?: string
     min?: string
 }
 
-export interface UploadInput extends InputArgs<string[]> {
-    files?: string[]
+export interface UploadInput<T> extends InputArgs {
+    type: "upload"
+    files?: string[],
+    api: (arg: { image: any, name: string }) => Promise<T>
 }
 
-export interface RadioInput extends InputArgs<boolean> {
+export interface RadioInput extends InputArgs {
+    type: "radio"
     name: string
 }
 
-export interface BooleanInput extends InputArgs<boolean> {
+export interface BooleanInput extends InputArgs {
+    type: "boolean"
     mustBeTrue?: boolean
 }
 
-export interface ComboInput<T> extends InputArgs<T> {
+export interface ComboInput<T> extends InputArgs {
+    type: "combo"
     visualName: keyof T,
     valueName: keyof T,
     values: T[]
@@ -64,15 +71,39 @@ export interface Input<T> {
     set: (value: T) => void
 }
 
-export type ObjInput<T extends Object> = {
-    [K in keyof T]: InputArgs<T[K]>
-}
+// export type ObjInput<T extends Object> = {
+//     [K in keyof T]: InputArgs<T[K]>
+// }
 
 
-export function build(def: InputArgs<string>) {
+export function build<T>(def: StringInput | NumberInput | DateInput | UploadInput<T> | RadioInput | BooleanInput | ComboInput<T>): Input<string> | Input<T | T[] | number | boolean> {
     switch (def.type) {
         case "string": {
             return string.stringInput(def, validator.validate("string"))
+        }
+        case "password": {
+            return string.stringInput(def, validator.validate("string"))
+        }
+        case "mail": {
+            return string.stringInput(def, validator.validate("string"))
+        }
+        case "number": {
+            return number.numberInput(def)
+        }
+        case "date": {
+            return date.date(def)
+        }
+        case "upload": {
+            return upload.uploadMulti(def)
+        }
+        case "radio": {
+            return radio.radio(def)
+        }
+        case "boolean": {
+            return boolean.booleanInput(def, validator.validate("boolean"))
+        }
+        case "combo": {
+            return combo.combo(def)
         }
     }
 }
--- index.ts
+++ index.ts
@@ -4,4 +4,6 @@ import * as _core from "./fields/_core"
 const core = _core.build
 
 
+const type = core({ type: "string", title: "cavallo" })
+
 export { core }
